0816dedf1f304dde8985c37d827511b76a12c155
mmozammel99 <mhfaryanemon@gmail.com>
2022-10-25 23:13:43 +0600

    fake data add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-const courses= require('./Data/Courses.json')
+const courses = require('./Data/Courses.json')
 app.use(cors())
 
 
@@ -11,6 +11,12 @@ app.get('/', (req, res) => {
 })
 app.get('/courses', (req, res) => {
     res.send(courses)
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+   
+    const course = courses.find(c => c.id == id )
+    res.send(course)
 })
 
 app.listen(port, () => {
